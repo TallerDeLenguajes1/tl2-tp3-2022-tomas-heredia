c0cc3b9ee0e4ee6b8dd2f8db5d9f9722db4ef651
Tomas <tomasheredia04@hotmail.com>
2022-09-28 11:42:41 -0300

    segundo commit

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,5 +1,21 @@
 ﻿using System;
-using MyApp; //namespace de la clase tipo
+
+using System.Net;
 using System.IO;  // para archivos csv
+using LectorCSV;
+using NLog;
+String DirCadetes = "Cadetes.csv";
+string DirClientes = "Clientes.csv";
+HelperCsv archivo = new HelperCsv();
+List<string[]> clientes = new List<string[]>();
+clientes = archivo.LeerCsv(DirClientes);
+
+foreach (string[] item in clientes)
+{
+    
+        Console.WriteLine(item);
+    
+}
+
 
 
